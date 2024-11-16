402b7fbe042d020ba7d9512ae86a26574bd34f54
Dataleak <logiebissett@outlook.com>
2024-11-16 11:53:23 +1030

    aaaaaaaaaaaaa

---
+++ songs/67LMD/scripts/modchart.hx
@@ -0,0 +1,9 @@
+
+
+function finaleBullshit(num:String){
+    if (num == '1')
+        FlxTween.tween(camHUD, {alpha: 0}, 1.2);
+    else if (num == '2') {
+        FlxTween.tween(camGame, {alpha: 0}, 0.4);
+    }
+}
